8910208ba95cf66412f84b707534372178eb0690
jasur28 <yuldashevj@gmail.com>
2023-05-05 12:12:18 +0500

    Add project files.

---
+++ DAL/CustomTimer.cs
@@ -0,0 +1,9 @@
+﻿namespace DAL
+{
+    public class CustomTimer
+    {
+        public int id { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+    }
+}
